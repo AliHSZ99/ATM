ef07b510a788c57fcf113748d680297e5ef8e1e4
Airi <airi@github.com>
2020-04-28 18:21:44 -0400

    PUSHING THIS BIIII

--- src/atm/ATM.java
+++ src/atm/ATM.java
@@ -17,7 +17,7 @@ public class ATM {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Hello");
+        System.out.println("Is this working");
     }
     
 }
