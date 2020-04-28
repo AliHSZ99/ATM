ece16eb4d80221d2a5905c2dfd55c430481cef3e
vincentbenesen <you@example.com>
2020-04-28 18:33:41 -0400

    Test push

--- src/atm/ATM.java
+++ src/atm/ATM.java
@@ -16,8 +16,7 @@ public class ATM {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        System.out.println("Test");
         System.out.println("ahhhhhh");
     }
-    
 }
