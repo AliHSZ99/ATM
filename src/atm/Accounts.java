e52fe858edfed51a641c5efa17c1743de01d11ff
vincentbenesen <you@example.com>
2020-05-09 12:47:19 -0400

    Added option String method and withdrawal receipt

--- src/atm/Accounts.java
+++ src/atm/Accounts.java
@@ -10,43 +10,58 @@ package atm;
  * @author Vincent Benesen, Jeremie Guerchon, Ali Zoubeidi
  */
 public class Accounts {
+
     private String username;
     private String password;
     private int money;
     private int deposit;
     private int withdraw;
-    
-    public Accounts (String username, String password, int money){
+
+    public Accounts(String username, String password, int money) {
         this.username = username;
         this.password = password;
         this.money = money;
     }
-    
-    public String getUserName(){
+
+    public String getUserName() {
         return username;
-     
+
     }
-    
-    public String getPassWord(){
+
+    public String getPassWord() {
         return password;
     }
-    
-    public int getMoney(){
+
+    public int getMoney() {
         return money;
     }
-    
-    public void setMoney(int newMoney){
+
+    public void setMoney(int newMoney) {
         money = newMoney;
     }
-        
-    @Override
-    public String toString(){
-        return "-------------------------------------" 
-                + "\n****WELCOME TO YOUR BANK ACCOUNT****" 
-                + "\nPlease choose for the following options "
-                +"\nPress (1) for Withdraw" 
-                + "\nPress (2) for Deposit" + "\nPress (3) for Check Balance" 
+
+    public String options() {
+        return "\nPlease choose for the following options "
+                + "\nPress (1) for Withdraw"
+                + "\nPress (2) for Deposit" + "\nPress (3) for Check Balance"
                 + "\nPress (4) for Transferring Money to other Account";
     }
-    
+
+    public String withdrawalReceipt() {
+        return "*******************************************"
+                + "\n *------------>>>J.A.V BANK<<<-----------*"
+                + "\n *----------Transaction  Receipt---------*"
+                + "\n*******************************************";
+    }
+
+    @Override
+    public String toString() {
+        return "-------------------------------------"
+                + "\n****WELCOME TO YOUR BANK ACCOUNT****"
+                + "\nPlease choose for the following options "
+                + "\nPress (1) for Withdraw"
+                + "\nPress (2) for Deposit" + "\nPress (3) for Check Balance"
+                + "\nPress (4) for Transferring Money to other Account";
+    }
+
 }
